77d6e575d338b74ffe29868b7e665335d17f3790
farrashafishz <bracenolimit@gmail.com>
2025-10-07 03:53:23 +0700

    test: ci/cd deploy (16)

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,6 +1,7 @@
-'use client';
-
-import Navbar from '@/layouts/landing/Navbar';
+import dynamic from 'next/dynamic';
+const Navbar = dynamic(() => import('@/layouts/landing/Navbar'), {
+  ssr: false,
+});
 import Jumbotron from './_containers/Jumbotron';
 import About from './_containers/About';
 
