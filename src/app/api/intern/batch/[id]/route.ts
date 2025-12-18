f142087b2856f033d5ab1fed99193d71e6179d91
farrashafishz <bracenolimit@gmail.com>
2025-12-18 18:51:51 +0700

    feat: iterasi 3

--- src/app/api/intern/batch/[id]/route.ts
+++ src/app/api/intern/batch/[id]/route.ts
@@ -1,4 +1,12 @@
 export const runtime = 'nodejs';
-import { internBatchSlugRouter } from '../../../route';
+import BatchServices from '@/Services/BatchServices';
+import { adminDb as db } from '@/lib/firebase-admin';
+import InternBatchSlugHandler from './handler';
 
-export const { PUT } = internBatchSlugRouter();
+const batchServices = new BatchServices(db);
+const internBatchSlugHandler = new InternBatchSlugHandler(batchServices);
+
+export const PUT = internBatchSlugHandler.PUT.bind(internBatchSlugHandler);
+// import { internBatchSlugRouter } from '../../../route';
+
+// export const { PUT } = internBatchSlugRouter();
