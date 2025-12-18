c945d11d03f91d05472ecce6af9eb90c9087b381
farrashafishz <bracenolimit@gmail.com>
2025-12-18 19:56:12 +0700

    feat: iterasi 3

--- src/app/api/intern/batch/stream/route.ts
+++ src/app/api/intern/batch/stream/route.ts
@@ -1,12 +1,12 @@
 export const runtime = 'nodejs';
-import InternServices from '@/Services/InternServices';
-import InternBatchStreamHandler from './handler';
-import { adminDb as db } from '@/lib/firebase-admin';
+// import InternServices from '@/Services/InternServices';
+// import InternBatchStreamHandler from './handler';
+// import { adminDb as db } from '@/lib/firebase-admin';
 
-const internServices = new InternServices(db);
-const internBatchStreamHandler = new InternBatchStreamHandler(internServices);
+// const internServices = new InternServices(db);
+// const internBatchStreamHandler = new InternBatchStreamHandler(internServices);
 
-export const GET = internBatchStreamHandler.GET.bind(internBatchStreamHandler);
+// export const GET = internBatchStreamHandler.GET.bind(internBatchStreamHandler);
 
-// import { internBatchStreamRouter } from '../../../route';
-// export const { GET } = internBatchStreamRouter();
+import { internBatchStreamRouter } from '../../../routers';
+export const { GET } = internBatchStreamRouter();
