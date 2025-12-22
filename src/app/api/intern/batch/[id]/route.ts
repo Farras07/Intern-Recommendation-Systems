1dc0d2ba2935ae45f7e9903be82fa30d2d05b6d5
farrashafishz <bracenolimit@gmail.com>
2025-12-22 09:07:24 +0700

    feat: iterasi 3

--- src/app/api/intern/batch/[id]/route.ts
+++ src/app/api/intern/batch/[id]/route.ts
@@ -1,7 +1,8 @@
 // app/api/intern/batch/[id]/route.ts
 export const runtime = 'nodejs';
-import { internBatchSlugRouter } from '@/app/api/routers';
+// import { internBatchSlugRouter } from '@/app/api/routers';
 
 export async function PUT(req: Request, ctx: any) {
+  const { internBatchSlugRouter } = await import('@/app/api/routers');
   return internBatchSlugRouter().PUT(req, ctx);
 }
--- src/app/api/intern/batch/route.ts
+++ src/app/api/intern/batch/route.ts
@@ -1,19 +1,23 @@
 // app/api/intern/batch/route.ts
 export const runtime = 'nodejs';
 
-import { internBatchRouter } from '@/app/api/routers';
+// import { internBatchRouter } from '@/app/api/routers';
 
 // export const { POST, GET, PUT, DELETE } = internBatchRouter();
 
 export async function POST(req: Request, ctx: any) {
+  const { internBatchRouter } = await import('@/app/api/routers');
   return internBatchRouter().POST(req, ctx);
 }
 export async function GET(req: Request, ctx: any) {
+  const { internBatchRouter } = await import('@/app/api/routers');
   return internBatchRouter().GET(req, ctx);
 }
 export async function PUT(req: Request, ctx: any) {
+  const { internBatchRouter } = await import('@/app/api/routers');
   return internBatchRouter().PUT(req, ctx);
 }
 export async function DELETE(req: Request, ctx: any) {
+  const { internBatchRouter } = await import('@/app/api/routers');
   return internBatchRouter().DELETE(req, ctx);
 }
